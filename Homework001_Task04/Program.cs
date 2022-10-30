809de881efe2a604df6591ea47c57ec36b3cff6f
Sergey Posokhov <posokhov_pit@mail.ru>
2022-10-30 22:11:48 +0300

    feat: add Homework001_Task04

---
+++ Homework001_Task04/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = Math.Max(a, b);
+Console.WriteLine("максимальное число будет " + Math.Max(max, c));
