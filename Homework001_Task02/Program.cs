31c45e32eaf6558406c94a33d00be982458576ee
Sergey Posokhov <posokhov_pit@mail.ru>
2022-10-31 14:45:14 +0300

    feat: updated Homework 01

--- Homework001_Task02/Program.cs
+++ Homework001_Task02/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 Console.WriteLine("Введите первое число a: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число b: ");
+Console.Write("Введите второе число b:  ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("max = " + Math.Max(a, b));
