4bec68b2a0ee68b017cca07ac873ecdd4dd48aa6
Sergey Posokhov <posokhov_pit@mail.ru>
2022-10-31 13:18:34 +0300

    feat: add Homework001 Task06

---
+++ Homework001_Task06/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите какое-нибудь число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
